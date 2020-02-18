7a85b9666017feeb139cbae14f9c012c52644054
Xerenz <martingeo15@gmail.com>
2020-02-19 00:26:37 +0530

    basic structure

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyABZm6Z_UOFebgfTZCarocPQEErmn9kP7U",
+    authDomain: "miniproject-266317.firebaseapp.com",
+    databaseURL: "https://miniproject-266317.firebaseio.com",
+    projectId: "miniproject-266317",
+    storageBucket: "miniproject-266317.appspot.com",
+    messagingSenderId: "701603913533",
+    appId: "1:701603913533:web:86cc4f92fa8529c7716f54",
+    measurementId: "G-5CJF8TW6ML"
+  };
+  
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+console.log(application.name)
+
+$(document).ready(function() {
+	console.log("ready");
+});
